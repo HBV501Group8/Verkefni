164e1403787be8f7efa22372b8731c5452122e22
Þorsteinn Sigurðsson <ths251@hi.is>
2020-10-27 16:50:56 +0000

    Uppfæra ForCast hlutinn og repository,service

--- Efnahagsspa/Efnahagsspa/src/main/java/is/hi/hbv501GEfnahagsspa/repositories/ForecastRepository.java
+++ Efnahagsspa/Efnahagsspa/src/main/java/is/hi/hbv501GEfnahagsspa/repositories/ForecastRepository.java
@@ -2,7 +2,9 @@ package is.hi.hbv501GEfnahagsspa.repositories;
 
 import is.hi.hbv501GEfnahagsspa.Entities.ForeCast;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ForecastRepository extends JpaRepository<ForeCast,Long> {
@@ -11,4 +13,7 @@ public interface ForecastRepository extends JpaRepository<ForeCast,Long> {
     ForeCast save(ForeCast foreCast);
     void delete(ForeCast foreCast);
     Optional<ForeCast> findById(long id);
+    List<ForeCast> findAll();
+    ForeCast findByForeCastName(String foreCastName);
+    ForeCast getForeCast();
 }
